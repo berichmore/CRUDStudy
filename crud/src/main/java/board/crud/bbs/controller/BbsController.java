4b7d4fd4a549a8ecdc9221917d006cfc2e60dcae
coding-faster <berichmore@naver.com>
2025-05-06 01:20:08 +0900

    dkdfkjdskfjsdkfj

---
+++ crud/src/main/java/board/crud/bbs/controller/BbsController.java
@@ -0,0 +1,26 @@
+package board.crud.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping
+@CrossOrigin("/*")
+public class BbsController {
+
+
+
+    @GetMapping()
+    public String start(){
+        return "/header/header";
+    }
+
+    @GetMapping("jay")
+    public String jayBranch(){
+        return "/jay/jaySession";
+    }
+
+}
