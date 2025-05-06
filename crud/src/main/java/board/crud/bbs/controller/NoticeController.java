d3ac9a70aba68d39ac44ceeb7b39a874a6972914
coding-faster <berichmore@naver.com>
2025-05-06 20:10:44 +0900

    새로시작

--- crud/src/main/java/board/crud/bbs/controller/NoticeController.java
+++ crud/src/main/java/board/crud/bbs/controller/NoticeController.java
@@ -4,24 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping
+@RequestMapping("/notice")
 @CrossOrigin("/*")
-public class BbsController {
+public class NoticeController {
 
 
 
-    @GetMapping()
+    @GetMapping("/header")
     public String start(){
         return "/header/header";
     }
 
-    @GetMapping("jay")
+    @GetMapping("/jay")
     public String jayBranch(){
         return "/jay/jaySession";
     }
     //너무짜증나ㅇㅇㅇ
+    @GetMapping({"/",""})
+    public String notice(){
+        return "/notice/list";
+    }
+
 
 }
